5363b7c12e29292ff9a460c9f4d5296272f6d0ad
Anostosia117 <jigsa@yandex.ru>
2022-04-06 19:28:19 +0300

    корректировки

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,25 +7,29 @@ public class Main {
         // Задание 1
 
         for (int i = 1; i <= 10; i++) {
-            System.out.print(i);
+            System.out.print(i + " ");
         }
         System.out.println();
         int start = 10;
         while ( start >= 1) {
-            System.out.print(start);
+            System.out.print(start + " ");
             start --;
         }
         System.out.println();
 
         // Задание 2
 
-        int friday = 3;
-        for (friday = 3; friday <= 31; friday += 7) {
+        for (int friday = 3; friday <= 31; friday += 7) {
                 System.out.println("Сегодня пятница, " + friday + "-е число. Необходимо подготовить отчет.");
         }
 
         // Задание 3
 
 
-    }
-}
+        for
+            }
+        }
+        }
+
+
+
