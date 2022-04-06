4bd51ce2f973b635f890da50cc6836df6fe685be
Anostosia117 <jigsa@yandex.ru>
2022-04-06 17:10:42 +0300

    задание 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,17 +4,28 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Р—Р°РґР°РЅРёРµ 1
+        // Задание 1
 
-        int i =1;
-        for (i = 1; i <= 10; i++) {
-            System.out.println(i);
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i);
         }
+        System.out.println();
         int start = 10;
         while ( start >= 1) {
-            System.out.println(start);
+            System.out.print(start);
             start --;
         }
+        System.out.println();
+
+        // Задание 2
+
+        int friday = 3;
+        for (friday = 3; friday <= 31; friday += 7) {
+                System.out.println("Сегодня пятница, " + friday + "-е число. Необходимо подготовить отчет.");
+        }
+
+        // Задание 3
+
 
     }
 }
