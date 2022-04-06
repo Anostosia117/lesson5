adc4917537536fbcaa57c010ffadb5399a6263b3
Anostosia117 <jigsa@yandex.ru>
2022-04-06 16:31:49 +0300

    задание 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // Задание 1
+
+        int i =1;
+        for (i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        int start = 10;
+        while ( start >= 1) {
+            System.out.println(start);
+            start --;
+        }
+
     }
 }
